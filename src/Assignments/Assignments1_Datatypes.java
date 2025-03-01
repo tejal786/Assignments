43b447be0c69656224965c304f7a0b811c7727cb
tejal dholakia <tejalrdholakia@gmail.com>
2025-03-01 15:48:07 +0530

    Assignment

---
+++ src/Assignments/Assignments1_Datatypes.java
@@ -0,0 +1,14 @@
+package Assignments;
+
+public class Assignments1_Datatypes {
+
+	public static void main(String[] args) {
+		//1. Temperature of a city in degrees Celsius: 25.5
+		float temperature = 25.5f;
+		System.out.println("Temperature of a city in degrees Celsius:"+temperature);
+		
+		
+	
+	}
+
+}
